58154c60ed4fcb0cba7d0529766536c9af6c3d8e
bgrochal <bartek.grochal@gmail.com>
2017-11-30 23:31:51 +0100

    Integration with the Configuration Server.

--- src/main/java/pl/edu/agh/iet/dts/edge/EdgeServer.java
+++ src/main/java/pl/edu/agh/iet/dts/edge/EdgeServer.java
@@ -1,12 +1,16 @@
 package pl.edu.agh.iet.dts.edge;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Bartłomiej Grochal
  */
+@SpringBootApplication
 public class EdgeServer {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(EdgeServer.class, args);
     }
 
 }
