337971ae3e35f66fab627f0898c96f0e5090f288
bgrochal <bartek.grochal@gmail.com>
2017-12-01 17:45:07 +0100

    Integration with the Eureka service discovery.

--- src/main/java/pl/edu/agh/iet/dts/edge/EdgeServer.java
+++ src/main/java/pl/edu/agh/iet/dts/edge/EdgeServer.java
@@ -2,11 +2,13 @@ package pl.edu.agh.iet.dts.edge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Bartłomiej Grochal
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class EdgeServer {
 
     public static void main(String[] args) {
