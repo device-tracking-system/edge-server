41fd40bbb5f67c3e087876c50776dbcfd5bd99e6
bgrochal <bartek.grochal@gmail.com>
2017-12-02 20:26:03 +0100

    Integration with the Zuul proxy.

--- src/main/java/pl/edu/agh/iet/dts/edge/EdgeServer.java
+++ src/main/java/pl/edu/agh/iet/dts/edge/EdgeServer.java
@@ -3,12 +3,14 @@ package pl.edu.agh.iet.dts.edge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author Bartłomiej Grochal
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableZuulProxy
 public class EdgeServer {
 
     public static void main(String[] args) {
