906773baace72355f7610a3490cbe25921ebaba1
bgrochal <bartek.grochal@gmail.com>
2017-12-03 23:38:52 +0100

    Integration with the Google OAuth2 authentication.

--- src/main/java/pl/edu/agh/iet/dts/edge/EdgeServer.java
+++ src/main/java/pl/edu/agh/iet/dts/edge/EdgeServer.java
@@ -2,8 +2,12 @@ package pl.edu.agh.iet.dts.edge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * @author Bartłomiej Grochal
@@ -11,10 +15,32 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
-public class EdgeServer {
+@EnableOAuth2Sso
+public class EdgeServer extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(EdgeServer.class, args);
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+                .authorizeRequests()
+                    .antMatchers("/", "/app", "/app/components/login", "/app/components/unauthenticated", "/login**",
+                                 "/index.html", "/templates/login.html", "/templates/unauthenticated.html",
+                                 "/angularMain.js", "/router.js", "/controller/indexController.js", "/controller/loginController.js",
+                                 "/webjars/**")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated()
+                .and()
+                    .logout()
+                    .logoutSuccessUrl("/")
+                    .permitAll()
+                .and()
+                    .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 
 }
